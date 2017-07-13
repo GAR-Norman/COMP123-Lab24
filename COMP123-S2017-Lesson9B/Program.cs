4c3d0efaeefb97ad96bcecfea056c9f4a73c341b
HRN <HRN@LAPTOP-47BNNCUR>
2017-07-13 16:50:41 -0400

    added appropriate supervillian attributes to the driver class

--- COMP123-S2017-Lesson9B/Program.cs
+++ COMP123-S2017-Lesson9B/Program.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
  * Name: Gabriel Norman
  * Date: July 11, 2017
  * Description: Lab 24 for Comp123
- * Version: 0.2 Created the Supervillian class and the IHasMaliceClass
+ * Version: 0.3 Created the Supervillian object and populated it in the driver class
  */
 
 namespace COMP123_S2017_Lesson9B
@@ -25,13 +25,13 @@ namespace COMP123_S2017_Lesson9B
             Console.WriteLine(superHero.ToString());
             Console.WriteLine("Karma:" + superHero.Karma);
 
-           /* SuperHero superHero = new SuperHero("Super Dude", 20);
-            superHero.AddPower("Spider Climbing", 50);
-            superHero.AddPower("Webcasting", 40);
-            superHero.AddPower("Invisibility", 40);
-            superHero.AddPower("Super strength", 40);
+            SuperVillian superVillian = new SuperVillian("Evil Dude", 40);
+            superVillian.AddPower("Mocking the Elderly", 10);
+            superVillian.AddPower("Loitering", 10);
+            superVillian.AddPower("Eye Lasers", 40);
+            superVillian.AddPower("Volcanic Sneeze", 50);
             Console.WriteLine(superHero.ToString());
-            Console.WriteLine("Karma:" + superHero.Karma); */
+            Console.WriteLine("Malice:" + superVillian.Malice); 
         }
     }
 }
